1f7e9f96ea79a63b0253b9ae114d64ec98cc22b5
Namuco <karlcedrick.namuco@tup.edu.ph>
2025-02-25 21:24:21 +0800

    slight changes

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,7 +1,7 @@
 fetch("/api/message")
   .then((response) => response.json())
   .then((data) => {
-    console.log("Message from server:", data.message); // Logs to console
+    console.log("Message from server:", data.message); 
   })
   .catch((error) => console.error("Error fetching message:", error));
 
@@ -9,7 +9,7 @@ function happyPlaylist(){
     const spotifyContainer = document.getElementById('spotifyContainer');
     const spotifyPlayer = document.getElementById("spotifyPlayer");
 
-    const playlistId = "37i9dQZF1DXdPec7aLTmlC?si=6c1ded227ca146e7"; // Example playlist ID
+    const playlistId = "37i9dQZF1DXdPec7aLTmlC?si=6c1ded227ca146e7"; 
     spotifyPlayer.src = `https://open.spotify.com/embed/playlist/${playlistId}`;
     spotifyContainer.style.display = "block";
 
@@ -19,7 +19,7 @@ function sadPlaylist(){
     const spotifyContainer = document.getElementById('spotifyContainer');
     const spotifyPlayer = document.getElementById("spotifyPlayer");
 
-    const playlistId = "37i9dQZF1DWSqBruwoIXkA?si=e5ccfab6ecf240ba"; // Example playlist ID
+    const playlistId = "37i9dQZF1DWSqBruwoIXkA?si=e5ccfab6ecf240ba"; 
     spotifyPlayer.src = `https://open.spotify.com/embed/playlist/${playlistId}`;
     spotifyContainer.style.display = "block";
 }
@@ -28,7 +28,7 @@ function lovePlaylist(){
     const spotifyContainer = document.getElementById('spotifyContainer');
     const spotifyPlayer = document.getElementById("spotifyPlayer");
 
-    const playlistId = "37i9dQZF1DX4cx0q5Jn2vp?si=d0694c2e28fc47a7"; // Example playlist ID
+    const playlistId = "37i9dQZF1DX4cx0q5Jn2vp?si=d0694c2e28fc47a7";
     spotifyPlayer.src = `https://open.spotify.com/embed/playlist/${playlistId}`;
     spotifyContainer.style.display = "block";
 }
@@ -37,7 +37,7 @@ function relaxedPlaylist(){
     const spotifyContainer = document.getElementById('spotifyContainer');
     const spotifyPlayer = document.getElementById("spotifyPlayer");
 
-    const playlistId = "3l6b0zuXjgyPxLK6PIAqED?si=c1e5e75568704477"; // Example playlist ID
+    const playlistId = "3l6b0zuXjgyPxLK6PIAqED?si=c1e5e75568704477";
     spotifyPlayer.src = `https://open.spotify.com/embed/playlist/${playlistId}`;
     spotifyContainer.style.display = "block";
 }
@@ -46,7 +46,7 @@ function pumpedPlaylist(){
     const spotifyContainer = document.getElementById('spotifyContainer');
     const spotifyPlayer = document.getElementById("spotifyPlayer");
 
-    const playlistId = "12T717v2KDBShceyjiZcMC?si=875ab811c37142e9"; // Example playlist ID
+    const playlistId = "12T717v2KDBShceyjiZcMC?si=875ab811c37142e9";
     spotifyPlayer.src = `https://open.spotify.com/embed/playlist/${playlistId}`;
     spotifyContainer.style.display = "block";
 }
